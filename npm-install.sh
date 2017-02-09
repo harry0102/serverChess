1afbbdd91a8590a10cf426c9cc902284e0a4d1a9
tony <994262676@qq.com>
2017-02-09 14:49:00 +0800

    删除第三方组件

---
+++ npm-install.sh
@@ -0,0 +1,5 @@
+cd ./game-server && cnpm install -d
+echo '============   game-server npm installed ============'
+cd ..
+cd ./web-server && cnpm install -d
+echo '============   web-server npm installed ============'
